bd2646bbb13f564ed1bdff45e2e5af3e7d08f20c
SofiaKal <128231824+SofiaKal@users.noreply.github.com>
2024-11-08 18:09:12 +0200

    Initial commit

---
+++ hiringProcess/src/main/java/com/example/hiringProcess/HiringProcessApplication.java
@@ -0,0 +1,14 @@
+package com.example.hiringProcess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringProcessApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HiringProcessApplication.class, args);
+	}
+
+}
