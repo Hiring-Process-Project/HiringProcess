fd5c00cd620216e895f5825a2d48129846d1c74f
Theognosia <127968532+Theognosia@users.noreply.github.com>
2025-03-05 14:49:17 +0200

    Εισαγωγη step για δοκιμή

--- hiringProcess/src/main/java/com/example/hiringProcess/JobAd/JobAdConfig.java
+++ hiringProcess/src/main/java/com/example/hiringProcess/JobAd/JobAdConfig.java
@@ -1,6 +1,7 @@
 package com.example.hiringProcess.JobAd;
 
 import com.example.hiringProcess.Interview.Interview;
+import com.example.hiringProcess.Step.Step;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +17,18 @@ public class JobAdConfig {
 
             JobAd jobAd1= new JobAd("title1","description1" , LocalDate.of(2012, 12, 12) ,"waiting");
 
-            jobAd1.setInterview(new Interview());
+            Interview interview = new Interview();
+
+
+
+            Step step1 = new Step("Technical Interview");
+            Step step2 = new Step("HR Interview");
+
+            // Πρόσθεσε τα steps στο interview
+            interview.addStep(step1);
+            interview.addStep(step2);
+
+            jobAd1.setInterview(interview);
             repository.save(jobAd1);
             System.out.println("JobAds saved.");
         };
